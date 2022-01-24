db435c5a72ac3e825f2f74dc5f4935273e1f2b49
miguel <mg20101994@gmail.com>
2022-01-23 19:44:32 -0500

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 // MySql
 const connection = mysql.createPool({
